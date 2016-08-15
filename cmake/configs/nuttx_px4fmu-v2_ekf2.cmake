9c706041d5e3db21577670d66ce8c2007999bdbd
Joseph Sullivan <jgs.424112@gmail.com>
2016-08-15 14:59:48 -0700

    Added smellocopter switch to Makefile, and project specific cmake file

--- cmake/configs/nuttx_px4fmu-v2_ekf2.cmake
+++ cmake/configs/nuttx_px4fmu-v2_ekf2.cmake
@@ -76,13 +76,6 @@ set(config_module_list
 	modules/land_detector
 
 	#
-	# Smellcopter eag control
-	#
-	examples/eag_control
-	examples/distance_filter
-	modules/ir_range_sensor
-
-	#
 	# Estimation modules (EKF/ SO3 / other filters)
 	#
 	# Too high RAM usage due to static allocations
